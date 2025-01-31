e51f8f295c2f1a7730c677add93f7a43cf9ea350
NourLassoued <lassoued.nour@esprit.tn>
2025-01-31 09:46:29 +0100

    init commit

---
+++ src/main/java/com/example/backendnourpfe/BackendnourpfeApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendnourpfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendnourpfeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendnourpfeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendnourpfe/BackendnourpfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendnourpfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendnourpfeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
