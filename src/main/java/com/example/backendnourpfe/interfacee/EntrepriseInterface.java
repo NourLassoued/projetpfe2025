d7dbdc62d1dd0fcbdf7b0d18198ca6d6aea90579
NourLassoued <lassoued.nour@esprit.tn>
2025-02-10 18:55:58 +0100

    init commit

--- src/main/java/com/example/backendnourpfe/interfacee/EntrepriseInterface.java
+++
@@ -1,8 +0,0 @@
-package com.example.backendnourpfe.interfacee;
-
-
-import com.example.backendnourpfe.classes.Entreprise;
-
-public interface EntrepriseInterface {
-    public Entreprise ajouterEntreprise(Entreprise entreprise);
-}
