cf9963859179ebfc75b7ae80e39bac9b45a380be
NourLassoued <lassoued.nour@esprit.tn>
2025-05-26 20:23:05 +0100

    ar733

--- src/main/java/com/example/backendnourpfe/service/FlouciService.java
+++ src/main/java/com/example/backendnourpfe/service/FlouciService.java
@@ -8,11 +8,11 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
+
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
