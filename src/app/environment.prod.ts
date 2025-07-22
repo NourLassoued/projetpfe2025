f8980b703691c33baca9a8b8134aee906043a269
NourLassoued <lassoued.nour@esprit.tn>
2025-07-22 22:52:10 +0100

    Frontbackend

--- src/app/environment.prod.ts
+++ src/app/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: (window as any).apiUrl || 'http://localhost:8088/nour' // fallback local si config.json pas chargé
+ apiUrl: 'http://192.168.16.140:31000/nour'
 };
