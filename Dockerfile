cd89516396353f70697cbbbcf194be5537e0201e
NourLassoued <lassoued.nour@esprit.tn>
2025-07-22 20:19:12 +0100

    Frontbackend

--- Dockerfile
+++ Dockerfile
@@ -11,8 +11,9 @@ COPY package*.json ./
 RUN npm install --force
 
 COPY . . 
+COPY src/main.prod.ts src/main.ts
+ng build --configuration=dynamic
 
-RUN ng build
 FROM nginx:latest
 RUN apt-get update && apt-get upgrade -y
 COPY nginx.conf /etc/nginx/conf.d/default.conf
---
+++ src/main.prod.ts
@@ -0,0 +1,14 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+
+fetch('/assets/config/config.json')
+  .then(res => res.json())
+  .then(config => {
+    (window as any).apiUrl = config.apiUrl;
+    platformBrowserDynamic().bootstrapModule(AppModule)
+      .catch(err => console.error(err));
+  })
+  .catch(err => {
+    console.error('Erreur lors du chargement de config.json', err);
+  });
