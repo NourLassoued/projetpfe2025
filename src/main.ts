eb4dae8c32a36f392f2dc151196b96ab0f8982f9
NourLassoued <lassoued.nour@esprit.tn>
2025-07-22 17:04:52 +0100

    Frontbackend

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+fetch('/assets/config/config.json')
+  .then(res => res.json())
+  .then(config => {
+    (window as any).apiUrl = config.apiUrl;
+    platformBrowserDynamic().bootstrapModule(AppModule)
+      .catch(err => console.error(err));
+  })
+  .catch(err => {
+    console.error('Erreur lors du chargement de config.json', err);
+  });
